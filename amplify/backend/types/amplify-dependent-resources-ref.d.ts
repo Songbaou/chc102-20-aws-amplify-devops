b50cbe7cf3ea1dc43b05cf48e3f85f8d46aceab0
Songbaou <wi121215@gmail.com>
2023-09-26 14:43:18 +0800

    pull ver1.0

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "chc10220awsamplifyde1a4a90af": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
